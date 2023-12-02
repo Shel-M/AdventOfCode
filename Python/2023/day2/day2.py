e8818084a3a8ddb6b77802a0fbc1171ceb99bf19
Sheldon M. <blackshadow606@yahoo.com>
2023-12-02 01:51:56 -0700

    Day 2 complete, commented

--- Python/2023/day2/day2.py
+++ Python/2023/day2/day2.py
@@ -7,24 +7,28 @@ def proc(line):
     global sum_1
     global sum_2
     global maximums
-    minimums = {"red": 0, "green": 0, "blue": 0}
 
+    minimums = {"red": 0, "green": 0, "blue": 0}
     line = line.split(":")
-    game = line[0][5:]
-    sets = line[1].split(";")
+    game = line[0][5:] # Save game number
+    sets = line[1].split(";") # Break up game sets
     possible = True
-    for set in sets:
-        vals = set.split(",")
-        pairs: dict[str, int] = {}
+
+    for set in sets: 
+        pairs: dict[str, int] = {} 
+
+        vals = set.split(",") # Extract colors
         for val in vals:
-            val = val.strip().split()
-            pairs.update({str(val[1]): int(val[0])})
-        for k in pairs.keys():
-            if maximums.get(k) < pairs.get(k): possible = False
+            val = val.strip().split() # Break the colors from their values
+            pairs.update({str(val[1]): int(val[0])}) # and save in a dictionary
+        for k in pairs.keys(): # Go over each color
+            # Check if each game is possible by getting the values matching the key in the dictionary
+            if maximums.get(k) < pairs.get(k): possible = False 
+            # Determine what the minimum shown for a given color is
             if minimums.get(k) < pairs.get(k): minimums[k] = pairs.get(k)
     if possible:
         sum_1 += int(game)
-    print(minimums)
+    # Multiply minimum values together
     power =  1
     for n in minimums.values(): power *= n
     sum_2 += power
